a8631b33dd8885a39d4dc0f0201c7762baf2d0a8
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-03-01 22:30:50 +0630

    updated navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <nav
-      className="rounded border-gray-200 px-2 py-2.5 dark:bg-gray-800 sm:px-4"
-      aria-label="navbar">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div class="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex">
           <svg
             version="1.0"
@@ -36,103 +36,90 @@ const NavBar = () => {
             <span className="whitespace-nowrap text-lg font-semibold dark:text-white">
               katakana
             </span>
-            {/* <div className="md:-mr-20">
-              {
-                <>
-                  <span className="text-sm text-gray-400">ー Welcome</span>{' '}
-                  <span className="rounded-full bg-indigo-200 p-2 text-sm text-indigo-500">
-                    hpn{' '}
-                  </span>
-                </>
-              }
-            </div> */}
+            <div className="md:-mr-20">
+              <span className="text-sm text-gray-400">ー Welcome</span>{' '}
+              <span className="rounded-full bg-indigo-200 p-2 text-sm text-indigo-500">
+                student
+              </span>
+            </div>
           </div>
         </Link>
-        <div className="md:order-2">
-          <div className="flex items-center">
-            {/* <select className="mr-2 rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-indigo-500 dark:focus:ring-indigo-500">
-              <option value="en-US">English</option>
-              <option value="ja-JP">日本語</option>
-              <option value="my-MM">မြန်မာ</option>
-            </select> */}
-            {
-              //   <NavLink to="/logout">
-              //     <button
-              //       type="button"
-              //       className="mr-3 rounded-lg bg-transparent px-3 py-2 text-center text-sm font-medium text-indigo-700 hover:bg-indigo-700 hover:text-white focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 md:mr-0">
-              //       Logout
-              //       <svg
-              //         xmlns="http://www.w3.org/2000/svg"
-              //         className="inline h-5 w-5"
-              //         fill="none"
-              //         viewBox="0 0 24 24"
-              //         stroke="currentColor">
-              //         <path
-              //           strokeLinecap="round"
-              //           strokeLinejoin="round"
-              //           strokeWidth={2}
-              //           d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              //         />
-              //       </svg>
-              //     </button>
-              //   </NavLink>
-            }
-          </div>
-
-          <button
-            data-collapse-toggle="mobile-menu-4"
-            type="button"
-            className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-            aria-controls="mobile-menu-4"
-            aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"></path>
-            </svg>
-            <svg
-              className="hidden h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"></path>
-            </svg>
-          </button>
-        </div>
-        <div
-          className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
-          id="mobile-menu-4">
-          <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-3 md:text-sm md:font-medium">
+        <button
+          data-collapse-toggle="mobile-menu"
+          type="button"
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mobile-menu-2"
+          aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"></path>
+          </svg>
+          <svg
+            class="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"></path>
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <NavLink
                 to="/learn"
                 className="block rounded py-2 px-3 md:rounded-3xl"
                 aria-current="page">
-                Learn
+                Learn{' '}
+                {pathname === '/learn' && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 inline"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z" />
+                  </svg>
+                )}
               </NavLink>
             </li>
             <li>
               <NavLink
                 to="/practice"
                 className="block rounded py-2 px-3 md:rounded-3xl">
-                Practice
+                Practice{' '}
+                {pathname === '/practice' && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 inline"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                  </svg>
+                )}
               </NavLink>
             </li>
             <li>
               <NavLink
                 to="/favorites"
                 className="block rounded py-2 px-3 md:rounded-3xl">
-                Favorites
+                About
               </NavLink>
+            </li>
+            <li>
+              <select className="mr-2 rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-indigo-500 dark:focus:ring-indigo-500">
+                <option value="en-US">English</option>
+                <option value="ja-JP">日本語</option>
+                <option value="my-MM">မြန်မာ</option>
+              </select>
             </li>
           </ul>
         </div>
