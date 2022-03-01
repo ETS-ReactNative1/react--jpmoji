b17f801cd508e0157109c86b999695aaa68e43d3
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-03-01 14:41:23 +0630

    installed flowbite

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <h1 className="text-3xl text-red-400 font-bold underline">Hello world!</h1>
+  );
+};
+
+export default App;
